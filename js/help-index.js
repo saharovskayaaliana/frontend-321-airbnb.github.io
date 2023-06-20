74aee6c22744e4c39ba202c65adf29a455c34b6f
Alina0404 <sakharovskaya534@gmail.com>
2023-06-20 23:45:10 +0800

    Доработали карточки,прикрутили логику

--- js/help-index.js
+++ js/help-index.js
@@ -1,3 +1,12 @@
+/* skeletons */
+const allSkeletons = document.querySelectorAll('.skeleton');
+
+window.addEventListener('load', () => {
+    allSkeletons.forEach(element => {
+        element.classList.remove('skeleton');
+    })
+})
+
 /*dropdown*/
 const dropDownButton = document.querySelector('.dropdown-button');
 const dropDownMenu = document.querySelector('.dropdown-menu');
@@ -71,39 +80,68 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs-content__active');
     }
 })
-
+/*слайдер */
+/* init swiper*/
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+        autoplay: {
+            delay: 2000,
+        },
+    });
+}
 /*fetch data*/
-function fetchData() {
-    fetch('../data.json')
-        .then(res => res.json())
-        .then(data => console.log(`data`, data))
-}
-function renderData() {
-    const products = fetchData();
-    console.log(`products`, products);
+const fetchData = () => {
+    return fetch('../data.json')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return res.json();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            return [];
+        });
+};
 
-    results.forEach(product => {
-        const div = document.createElement('div');//создаёт div
+const renderData = async () => {
+    try {
+        const products = await fetchData();
+        const swiperWrapper = document.querySelector('.swiper-wrapper');
 
-        div.classList.add('card-wrapper');
-        div.innerHTML = `
-        <a class="card-link" href="#" target="_blank" rel="noopener noreferrer nofollow"></a>
-        <div class="card">
-          <div class="card-image">
-            <img src="${product.imgSrc}" alt="image">
-          </div>
+        if (swiperWrapper) {
+            products.forEach(product => {
+                const div = document.createElement('div');
+                div.classList.add('swiper-slide');
 
-            <div class="card-text_descr text-secondary">${product.rating}</div>
-            <div class="card-text_config text-secondary">${product.name}</div>
-            <div class="card-text_date text-secondary">${product.price}</div>
-          </div>
-        `
+                div.innerHTML = `
+            <a class="card-link" href="help-details.html?id=${product.id}" target="_blank" rel="noopener noreferrer nofollow"></a>
+            <div class="card">
+              <div class="card-image">
+                <img src="${product.imgSrc}" alt="image">
+              </div>
+              <div class="card-text__descr text-secondary">${product.rating}</div>
+              <div class="card-text__config text-secondary">${product.name}</div>
+              <div class="card-text__date text-secondary">${product.price}</div>
+            </div>
+          `;
 
-        document.querySelector('.products-wrapper').appendChild(div);
-    })
-}
-
+                swiperWrapper.appendChild(div);
+            });
+        } else {
+            console.error('Element with class "swiper-wrapper" not found');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+    initSwiper();
+};
 
 document.addEventListener('DOMContentLoaded', renderData);
-
 
